d89f230e5ab85fcf12443c7ed145a5bc35e5422d
hafiz <hafizxfattah@gmail.com>
2022-07-11 10:09:09 +0700

    setup: ui web

---
+++ packages/components/General/Button/Button.tsx
@@ -0,0 +1,11 @@
+import styles from './BaseTemplate.module.scss';
+
+export interface IBaseTemplate {
+  sampleTextProp: string;
+}
+
+const BaseTemplate = ({sampleTextProp}: IBaseTemplate) => {
+  return <div className={styles.container}>{sampleTextProp}</div>;
+};
+
+export default BaseTemplate;
